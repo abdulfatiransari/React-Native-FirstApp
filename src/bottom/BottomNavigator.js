9a1ae1379aa3cb314a752bc67d93693264cb89b3
Abdul Fatir Ansari <abdulfatiransari2@gmail.com>
2023-06-14 18:07:42 +0500

    repo fixes

--- src/bottom/BottomNavigator.js
+++ src/bottom/BottomNavigator.js
@@ -1,31 +1,61 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Screen1 from './Screen1';
-import Screen2 from './Screen2';
-import Screen3 from './Screen3';
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Screen1 from "./Screen1";
+import Screen2 from "./Screen2";
+import Screen3 from "./Screen3";
 
 const Bottom = createBottomTabNavigator();
 const BottomNavigator = () => {
   return (
     <Bottom.Navigator>
-        <Bottom.Screen name="Screen1" component={Screen1} options={{headerShown: false, tabBarIcon:() => {
-            return(
-                <Image source={require('../../assets/favicon.png')} style={{width:20, height:20}} />
-            )
-        } }} />
-        <Bottom.Screen name="Screen2" component={Screen2} options={{headerShown: false, tabBarIcon:() => {
-            return(
-                <Image source={require('../../assets/favicon.png')} style={{width:20, height:20}} />
-            )
-        } }} />
-        <Bottom.Screen name="Screen3" component={Screen3} options={{headerShown: false, tabBarIcon:() => {
-            return(
-                <Image source={require('../../assets/favicon.png')} style={{width:20, height:20}} />
-            )
-        } }} />
+      <Bottom.Screen
+        name="Screen1"
+        component={Screen1}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require("../../assets/favicon.png")}
+                style={{ width: 20, height: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Bottom.Screen
+        name="Screen2"
+        component={Screen2}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require("../../assets/favicon.png")}
+                style={{ width: 20, height: 20 }}
+              />
+            );
+          },
+        }}
+      />
+      <Bottom.Screen
+        name="Screen3"
+        component={Screen3}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => {
+            return (
+              <Image
+                source={require("../../assets/favicon.png")}
+                style={{ width: 20, height: 20 }}
+              />
+            );
+          },
+        }}
+      />
     </Bottom.Navigator>
-  )
-}
+  );
+};
 
-export default BottomNavigator
+export default BottomNavigator;
